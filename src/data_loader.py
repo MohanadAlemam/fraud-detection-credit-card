86504a0df3e36ccd4697568714907dc84f92e00c
Mohanad Alemam <mohannad.alemam@gmail.com>
2025-10-17 12:16:42 +0100

    feat(data loader): create CSV data loader/ import function with error (e.g., type error and value errors) handling and docstring.

---
+++ src/data_loader.py
@@ -0,0 +1,24 @@
+# Initial required libraries
+import numpy as np
+import pandas as pd
+
+# 01. Data Loader Function
+def import_data(file_path : str, file_type: str= "csv"):
+    """
+    Imports a CSV file into a pandas DataFrame.
+
+    :param file_path: Path to the CSV file.
+    :param file_type: Type of file to load. Must be 'csv' (default).
+    :return: pd.DataFrame: Loaded data
+    """
+    try:
+        if file_type.lower() == "csv":
+            raw_data = pd.read_csv(file_path)
+        else:
+            raise TypeError("File type must be 'csv'")
+        if raw_data.empty:
+            raise ValueError("The CSV file is empty.")
+        return raw_data
+
+    except Exception as e:
+        raise ValueError(f"Failed to import data: {e}")
