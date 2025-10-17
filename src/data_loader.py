cd4735c178a86bcf5a9bd40561f9b99aff8dec95
Mohanad Alemam <mohannad.alemam@gmail.com>
2025-10-17 19:46:39 +0100

    feat(eda): enhance class balance function to also compute the count of the class instances.

--- src/data_loader.py
+++ src/data_loader.py
@@ -28,11 +28,20 @@ def import_data(file_path : str, file_type: str= "csv"):
 
 #02. Class Balance Function
 def class_balance(labeled_data : pd.DataFrame):
-    labeled_data = labeled_data.drop_duplicates()
+    """
+    Calculate the data class imbalance across the two classes.
 
+    :param labeled_data: Pandas DataFrame containing the data
+    :return: data frame containing % of each class in the data and count.
+    """
+    value_count = labeled_data["Class"].value_counts()
     imbalance_scores = labeled_data["Class"].value_counts()/len(labeled_data) * 100
     # Count each class instances and divide by the total number of instances
-    imbalance_scores = imbalance_scores.round(2)
-    imbalance_scores = imbalance_scores.rename('Percentage of the Class')
+    imbalance_scores = imbalance_scores.round(3)
 
-    return imbalance_scores
+    imbalance_scores = imbalance_scores.rename('Percentage of the Class')
+    df = pd.concat([value_count, imbalance_scores], axis=1)
+
+    return df
+
+#3. Assess Duplication
