7b8daf742d4cb64b24ee43c76b64618dbfba509a
Mohanad Alemam <mohannad.alemam@gmail.com>
2025-11-22 14:16:06 +0000

    fix (CatBoost bug): Fix OOF validation to handle CatBoost in pipelines
    CatBoost classifiers cannot be cloned or handled like standard sklearn estimators.
    Updated the off_validation function to properly handle CatBoost steps in pipelines
    by passing `cat_features` via step-specific parameters and using deepcopy, ensuring
    correct out-of-fold predictions.

--- src/model.py
+++ src/model.py
@@ -1,20 +1,23 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from catboost import CatBoostClassifier, CatBoostRegressor
 from sklearn.metrics import (classification_report,
                              average_precision_score,
                              roc_auc_score,
                              balanced_accuracy_score,
                              confusion_matrix,
-                             ConfusionMatrixDisplay, f1_score)
+                             ConfusionMatrixDisplay)
 from sklearn.model_selection import StratifiedKFold, cross_val_predict
 from sklearn.base import clone # to handle CatBoost or other models that sometimes break cloning).
+from sklearn.pipeline import Pipeline
+from copy import deepcopy
 
 
-# Out-of-fold (OOF) evaluation
-def oof_evaluation(model_dict: dict, X, y):
+# Out-of-fold (OOF) validation
+def oof_validation(model_dict: dict, X, y, categorical_features =None):
     """
-    Perform out-of-fold (OOF) evaluation for multiple classification models.
+    Perform out-of-fold (OOF) evaluation/ validation for multiple classification models.
 
     :param (dict): Dictionary of model_name: model_object pairs.
     :param (pd.DataFrame): Feature matrix.
@@ -24,18 +27,40 @@ def oof_evaluation(model_dict: dict, X, y):
     sorted by PR AUC in descending order.
     """
     cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=3479)
-
     compare_models = []
 
+    # 1. CatBoostClassifier.
     for model_name, model in model_dict.items():
-        # OOF predicted probabilities
-        oof_probs = cross_val_predict(clone(model, safe=False), # to handle CatBoost which breaks cloning
-                                  X,
-                                  y,
-                                  cv=cv,
-                                  n_jobs=-1,
-                                  method='predict_proba')[:,1]
+        # Checks and handle CatBoost or Pipelines whose final estimator is CatBoost
+        if isinstance(model, Pipeline) and isinstance(model.steps[-1][1], CatBoostClassifier):
+            oof_probs = np.zeros(len(y))
+            step_name = model.steps[-1][0] # get the classifier name
 
+            for train_idx, val_idx in cv.split(X, y):
+                X_train_fold, X_val_fold = X.iloc[train_idx], X.iloc[val_idx]
+                y_train_fold = y.iloc[train_idx]
+
+                est = deepcopy(model)
+                est.fit(
+                    X_train_fold,
+                    y_train_fold,
+                    **{f"{step_name}__cat_features": categorical_features,
+                       f"{step_name}__verbose": False},
+                    # ** Python are used for unpacking dictionaries into keyword arguments
+                )
+                oof_probs[val_idx] = est.predict_proba(X_val_fold)[:, 1]
+
+        # 2. Other classifiers
+        else:
+            estimator = clone(model)
+            # OOF predicted probabilities
+            oof_probs = cross_val_predict(
+                estimator,
+                X,
+                y,
+                cv=cv,
+                n_jobs=-1,
+                method='predict_proba')[:,1] # slice positive class probabilities
         # OOF predictions
         oof_preds = (oof_probs >= 0.5).astype(int) # coverts probabilities to labels
 
@@ -51,7 +76,6 @@ def oof_evaluation(model_dict: dict, X, y):
         metrics_df.drop(index=["accuracy", "macro avg", "weighted avg"], inplace=True)
         # Drop accuracy from index, we have balanced accuracy instead
         metrics_df.drop(columns=["support"], inplace=True)  # drop support from index, for simplicity
-
         metrics_df["val pr auc"] = pr_auc
         metrics_df["val roc auc"] = roc_auc
 
